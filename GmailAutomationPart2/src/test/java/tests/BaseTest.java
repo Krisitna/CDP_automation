ca2e817e956a833ca047b09a0a452ad8e895c99a
Kristina Tyavlovskaya <Kristina_Tyavlovskaya@epam.com>
2017-02-08 21:54:02 +0300

    Update GmailTest.java and BaseTest.java file

--- GmailAutomationPart2/src/test/java/tests/BaseTest.java
+++ GmailAutomationPart2/src/test/java/tests/BaseTest.java
@@ -6,9 +6,19 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
+import pages.DraftPage;
+import pages.HomePage;
+import pages.LoginPage;
+import pages.SendPage;
+
+
 public class BaseTest {
 	protected static WebDriver driver;
 	protected static final String startURL = "https://gmail.com";
+    protected LoginPage loginPage;
+	protected HomePage homePage;
+	protected DraftPage draftPage;
+	protected SendPage sendPage;
 
 	@BeforeClass
 	public void beforeClass() {
@@ -17,6 +27,10 @@ public class BaseTest {
 		DesiredCapabilities capabilities = DesiredCapabilities.chrome();
 		driver = new ChromeDriver(capabilities);
 		driver.get(startURL);
+        loginPage = new LoginPage(driver);
+		homePage = new HomePage(driver);
+		draftPage = new DraftPage(driver);
+		sendPage = new SendPage(driver);
 	}
 
 	@AfterClass
